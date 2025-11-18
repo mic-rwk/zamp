43b5ec21f690d5b21d0330cb6f0fb34891955ce5
Michał Rywak <michalrywak@gmail.com>
2025-11-18 19:38:36 +0100

    Edit Rotate command for graphics server

--- plugin/src/Interp4Rotate.cpp
+++ plugin/src/Interp4Rotate.cpp
@@ -30,8 +30,48 @@ const char* Interp4Rotate::GetCmdName() const {
     return ::GetCmdName();
 }
 
-bool Interp4Rotate::ExecCmd(AbstractScene &/*rScn*/, const char */*sMobObjName*/, AbstractComChannel &/*rComChann*/) {
-    std::cout << "[Rotate] ExecCmd  dla obiektu " << objName << "\n";
+bool Interp4Rotate::ExecCmd(AbstractScene & rScn, const char * sMobObjName, AbstractComChannel & rComChann) {
+    
+    AbstractMobileObj *pMob = rScn.FindMobileObj(objName.c_str());
+    if (!pMob) {
+        std::cerr << "[Interp4Rotate] Brak obiektu o nazwie: " << objName << "\n";
+        return false;
+    }
+
+    std::cout << "[Interp4Rotate] Obrót obiektu: " << objName
+         << " o kąt " << angleDegree << " wokół osi " << axisName
+         << " z prędkością " << angularSpeed << "°/s\n";
+
+    double step_deg = (angularSpeed > 0 ? 1.0 : -1.0);
+    double sleep_us = (1e6 * fabs(step_deg) / fabs(angularSpeed));
+    double rotated = 0.0;
+
+    double roll = pMob->GetAng_Roll_deg();
+    double pitch = pMob->GetAng_Pitch_deg();
+    double yaw = pMob->GetAng_Yaw_deg();
+
+    while (fabs(rotated) < fabs(angleDegree)) {
+        if (axisName == "OX")
+            roll += step_deg;
+        else if (axisName == "OY")
+            pitch += step_deg;
+        else if (axisName == "OZ")
+            yaw += step_deg;
+
+        pMob->SetAng_Roll_deg(roll);
+        pMob->SetAng_Pitch_deg(pitch);
+        pMob->SetAng_Yaw_deg(yaw);
+
+        std::ostringstream cmd;
+        cmd << "UpdateObj Name=" << objName
+            << " RotXYZ_deg=(" << roll << "," << pitch << "," << yaw << ")\n";
+        rComChann.Send(cmd.str());
+
+        usleep((useconds_t)sleep_us);
+        rotated += step_deg;
+    }
+
+    std::cout << "[Interp4Rotate] Zakończono obrót obiektu: " << objName << "\n";
     return true;
 }
 
